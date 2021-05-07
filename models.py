39eb6401dd1a711e2a78b7d2a0631e8be18cbb51
Eelolo <Eelolo@yandex.ru>
2021-05-07 21:31:56 +0300

    added users table

---
+++ models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25))
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(500), nullable=True)
