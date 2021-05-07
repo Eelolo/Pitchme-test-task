b7aac7665022d7db4d362956ea13efcf287184b8
Eelolo <Eelolo@yandex.ru>
2021-05-07 21:33:38 +0300

    added topics table

--- models.py
+++ models.py
@@ -10,3 +10,8 @@ class Users(db.Model):
     name = db.Column(db.String(25))
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(500), nullable=True)
+
+
+class Topics(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
