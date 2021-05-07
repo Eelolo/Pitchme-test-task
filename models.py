ad8ff8fd47796d06ffbc023f86fa4e8e52fe009e
Eelolo <Eelolo@yandex.ru>
2021-05-07 21:49:11 +0300

    added events model

--- models.py
+++ models.py
@@ -16,6 +16,16 @@ class Topics(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
 
+
 class Cities(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
+
+
+class Events(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.Text(1000))
+    start_at = db.Column(db.DateTime())
+    end_at = db.Column(db.DateTime())
+    city_id = db.Column(db.Integer, db.ForeignKey('cities.id'))
