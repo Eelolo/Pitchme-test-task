23e4906aa8588226925491edb8bbde94746c2955
Eelolo <Eelolo@yandex.ru>
2021-05-11 17:45:24 +0300

    login

--- app/views.py
+++ app/views.py
@@ -113,3 +113,29 @@ def signup():
         return redirect(url_for('auth.login'))
 
     return render_template('signup.html')
+
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        remember = True if request.form.get('remember') else False
+
+        user = Users.query.filter_by(email=email).first()
+
+        if not user or not check_password_hash(user.password, password):
+            flash('Please check your login details and try again')
+            return redirect(url_for('auth.login'))
+
+        login_user(user, remember=remember)
+        return redirect(url_for('main.index'))
+
+    return render_template('login.html')
+
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
