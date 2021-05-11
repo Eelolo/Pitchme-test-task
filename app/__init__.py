1b2a3b40f58b8ae95e83b06d9b56516d6f0df43a
Eelolo <Eelolo@yandex.ru>
2021-05-11 17:43:09 +0300

    registered flask login library

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 db = SQLAlchemy()
@@ -9,6 +10,7 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
     from .views import main_bp
     app.register_blueprint(main_bp)
@@ -31,6 +33,19 @@ def create_app():
     from .saved_filters.views import admin_bp
     app.register_blueprint(admin_bp)
 
+    from .views import auth_bp
+    app.register_blueprint(auth_bp)
+
     db.init_app(app)
+
+    from .users.model import Users
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return Users.query.get(int(user_id))
 
     return app
--- app/users/model.py
+++ app/users/model.py
@@ -1,7 +1,8 @@
 from app import db
+from flask_login import UserMixin
 
 
-class Users(db.Model):
+class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(25))
     email = db.Column(db.String(50), unique=True)
